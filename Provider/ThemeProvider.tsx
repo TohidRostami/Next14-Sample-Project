a2401a621d5d4c1b3d28c467b6209b183f6629d3
TohidRostamii <tohid@greenhabitat.at>
2025-01-25 14:22:15 +0330

    feat(fix): changed theme provider

--- Provider/ThemeProvider.tsx
+++ Provider/ThemeProvider.tsx
@@ -1,16 +1,10 @@
 "use client";
 import React from "react";
 import { useState } from "react";
-import {
-  Box,
-  createTheme,
-  MenuItem,
-  Select,
-  ThemeProvider,
-} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 import i18n from "@/Translation/i18next";
-import ResponsiveAppBar from "@/components/ResponsiveAppBar";
+import SideBar from "@/components/SideBar";
 
 export default function useCustomTheme({
   children,
@@ -35,31 +29,11 @@ export default function useCustomTheme({
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <Box
-        sx={{
-          width: "100%",
-          borderBottom: "1px solid lightgrey",
-          padding: "10px 10px",
-        }}
-      >
-        <Select
-          value={mode}
-          onChange={(event) => setMode(event.target.value as "light" | "dark")}
-          sx={{ fontSize: "0.8rem" }}
-        >
-          <MenuItem value="light">Light</MenuItem>
-          <MenuItem value="dark">Dark</MenuItem>
-        </Select>
-        <Select
-          defaultValue={i18n.language}
-          onChange={(e) => handleLanguageChange(e.target.value as "en" | "de")}
-          sx={{ fontSize: "0.8rem", marginLeft: "10px" }}
-        >
-          <MenuItem value="en">English</MenuItem>
-          <MenuItem value="de">German</MenuItem>
-        </Select>
-      </Box> */}
-      <ResponsiveAppBar />
+      <SideBar
+        mode={mode}
+        setMode={setMode}
+        handleLanguageChange={handleLanguageChange}
+      />
       {children}
     </ThemeProvider>
   );
